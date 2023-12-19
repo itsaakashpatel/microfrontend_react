937fdaf14f29b2913b15e275c1ca78733127fc3a
Aakash Patel <hi@aakashpatel.in>
2023-12-18 21:36:50 -0500

    Trying to fix remote url is not being used to serve remote js bundles

--- shared/webpack.config.js
+++ shared/webpack.config.js
@@ -1,9 +1,10 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
-const deps = require("./package.json").dependencies
+const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
+    uniqueName: "shared",
     publicPath: argv.mode === "development" ? "http://localhost:8081/" : "/",
   },
 
@@ -66,4 +67,4 @@ module.exports = (_, argv) => ({
       template: "./src/index.html",
     }),
   ],
-})
+});
