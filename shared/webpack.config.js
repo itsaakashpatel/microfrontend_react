83ede18778c0eb0b1808e72c825aff7b56e09a5a
Aakash Patel <aakashpatel25274@gmail.com>
2023-12-19 01:27:51 +0000

    If development mode then serve localhost

--- shared/webpack.config.js
+++ shared/webpack.config.js
@@ -1,10 +1,10 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
 
-const deps = require("./package.json").dependencies;
+const deps = require("./package.json").dependencies
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:8081/",
+    publicPath: argv.mode === "development" ? "http://localhost:8081/" : "/",
   },
 
   resolve: {
@@ -66,4 +66,4 @@ module.exports = (_, argv) => ({
       template: "./src/index.html",
     }),
   ],
-});
+})
